5f3f5b48e9f478c53f3a772f7f6b099b5e63a5b4
Sandrine N.PENDA <60267849+SandrinePENDA@users.noreply.github.com>
2020-08-17 17:50:02 +0200

    Backend of API with Laravel 7
    Backend of API with Laravel 7

---
+++ mes-fruits-exotik/database/migrations/2020_08_15_155342_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('photo_fruit');
+            $table->string('country_fruit');
+            $table->string('how_eating');
+            $table->string('buying_place');
+            $table->string('more_infos');
+            $table->string('author')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
